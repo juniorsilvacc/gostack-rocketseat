b956f5417f7a51219c7be1c18eff40f671f7ee6e
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-10-23 17:37:53 -0300

    relationship in models user and appointment

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -7,9 +7,9 @@ import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.get('/', (request, response) => {
+appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = appointmentsRepository.find();
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
@@ -29,7 +29,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     return response.json(appointment);
   } catch (err) {
-    return response.status(400).json({ message: err });
+    return response.status(400).json({ error: err.message });
   }
 });
 
