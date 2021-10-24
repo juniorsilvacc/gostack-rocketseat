3eed0c91970114f23830b3639d19615e8d1c81ef
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-10-24 18:12:39 -0300

    hash

--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { getRepository } from 'typeorm';
 
 import User from '../models/User';
@@ -20,10 +21,13 @@ class CreateUserService {
       throw new Error('Email address already used.');
     }
 
+    const SALT_FACTOR = 8;
+    const hashedPassword = await hash(password, SALT_FACTOR);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
