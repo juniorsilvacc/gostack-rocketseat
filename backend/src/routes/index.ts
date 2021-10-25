3730e939cef5d6ce9958892f327c6332559f9b64
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-10-24 21:25:37 -0300

    validating the credentials

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import appointmetsRouter from './appointments.routes';
+import sessionsRouter from './sessions.routes';
 import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmetsRouter);
+routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
 
 export default routes;
