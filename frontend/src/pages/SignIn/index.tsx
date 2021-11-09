d8450e5fc418051370c11b7d6a3f289371fc63ed
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-11-09 03:13:49 -0300

    adding and removing toasts

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -52,7 +52,11 @@ const SignIn: React.FC = () => {
       }
 
       //disparar um toast
-      addToast();
+      addToast({
+        type: 'error',
+        title: 'Erro na autenticação.',
+        description: 'Ocorreu um erro ao fazer login, cheque as credenciais.'
+      });
       
     }
   }, [signIn, addToast]);
